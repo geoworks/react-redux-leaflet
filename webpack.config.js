57622e256f853a4008e190e658c0ff5bd3ad5609
Sergey Burmisov <skypath@gmail.com>
2016-05-22 12:10:30 +0300

    prepare for publish

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'react-redux-leaflet.js',
+    library: 'react-redux-leaflet',
+    libraryTarget: 'commonjs',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js'],
+  },
+  externals: {
+    debounce: true,
+    immutable: true,
+    leaflet: true,
+    react: true,
+    'react-dom': true,
+    'react-immutable-proptypes': true,
+    'react-redux': true,
+    redux: true,
+    reselect: true,
+    events: true,
+  },
+};
