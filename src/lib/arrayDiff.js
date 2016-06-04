c136802ad505a84783050ecc23ceebfb2b50d633
Sergey Burmisov <skypath@gmail.com>
2016-06-04 13:33:02 +0300

    fix array diff

--- src/lib/arrayDiff.js
+++ src/lib/arrayDiff.js
@@ -1,4 +1,4 @@
-export default function arrayDiff(oldArray, newArray) {
+export default function arrayDiff(oldArray = [], newArray = []) {
   return {
     additions: newArray.filter(e => oldArray.indexOf(e) === -1),
     deletions: oldArray.filter(e => newArray.indexOf(e) === -1),
